c6db73a42bd099048127a6e8b6bc3742b6025423
sakshi-sn05 <sakshi.wankhade@sapienceanalytics.com>
2025-12-10 15:09:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -46,6 +47,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order +'!', icon="✅")
 
 
-import requests
+
 
 
